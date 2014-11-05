c769c9893a5a7cb94c9a64a14d8ed0fff4bca651
cheshirepuss <cheshirepuss42@gmail.com>
2014-11-05 10:10:42 +0100

    setup for prototype

---
+++ prototype/DungeonCollapse/app.ts
@@ -0,0 +1,21 @@
+﻿class Game {
+    element: HTMLElement;
+    constructor(element: HTMLElement) {
+        this.element = element;
+    }
+
+    start() {
+       
+    }
+
+    stop() {
+       
+    }
+}
+
+var game: Game;
+window.onload = () => {
+    var el = document.getElementById('content');
+    game = new Game(el);
+    game.start();
+};
